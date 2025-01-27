3e12fa36819975487f729036cb2458f274b0e236
Arthy04 <arthy.uthayarajah@gmail.com>
2025-01-27 19:35:31 +0100

    adding the e2e tests

--- cypress/E2E/1-getting-started/todo.cy.js
+++ cypress/E2E/1-getting-started/todo.cy.js
@@ -3,8 +3,8 @@
 // Welcome to Cypress
 // This spec file contains a variety of sample tests
 // for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
+// the power of writing tests in Cypress...
+
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
